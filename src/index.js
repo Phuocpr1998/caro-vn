0778cd6be784003c5c0f197710b41b15721b8b7e
phuocnv2 <phuocpr1998@gmail.com>
2019-10-21 15:17:25 +0700

    add react bootrap and react router

--- src/index.js
+++ src/index.js
@@ -2,15 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './content/index.css';
+// eslint-disable-next-line import/imports-first
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Game from './components/gameplay/Game';
 import rootReducer from './reducers';
+import LoginForm from './components/login/LoginForm';
+import RegisterForm from './components/register/RegisterForm';
 
 const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Game />
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <LoginForm />
+        </Route>
+        <Route path="/register">
+          <RegisterForm />
+        </Route>
+        <Route path="/play">
+          <Game />
+        </Route>
+      </Switch>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
