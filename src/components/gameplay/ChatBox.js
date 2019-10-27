075605e5b6f6fca6c7bd9063c490262e5d583e1a
Phước Nguyễn Văn <phuocpr1998@gmail.com>
2019-10-27 23:29:43 +0700

    update ui chat

--- src/components/gameplay/ChatBox.js
+++ src/components/gameplay/ChatBox.js
@@ -5,18 +5,31 @@ import React from 'react';
 import { Form, Button, Image } from 'react-bootstrap';
 
 import '../../content/scrollbar.css';
+import '../../content/Game.css';
 
-function getItemMessage(message) {
+function getItemMessage(index, message) {
   if (message.people !== undefined && message.people !== null) {
-    console.log(message);
     return (
-      <>
-        <Image rounded src={message.people.photo} />
-        {message.value}
-      </>
+      <ul className="message-item-1">
+        <li key={index} className="message-item">
+          <Image
+            className="message-item-img"
+            src={message.people.photo}
+            width="30px"
+            height="30px"
+          />
+          <span className="message-1">{message.value}</span>
+        </li>
+      </ul>
     );
   }
-  return <>{message.value}</>;
+  return (
+    <ul className="message-item-0">
+      <li key={index} className="message-item">
+        <span className="message-0">{message.value}</span>
+      </li>
+    </ul>
+  );
 }
 
 function ChatBox({
@@ -32,11 +45,7 @@ function ChatBox({
           <h5>Nhắn tin cho đối thủ</h5>
         </div>
         <div className="message-content scrollbar scrollbar-primary">
-          <ul>
-            {messages.map((value, index) => (
-              <li key={index}>{getItemMessage(value)}</li>
-            ))}
-          </ul>
+          {messages.map((value, index) => getItemMessage(index, value))}
         </div>
       </div>
       <div>
